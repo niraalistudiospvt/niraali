72ec942788a887ba7e87761a0a804037e4275932
gopalakustagi <83970270+gopalakustagi@users.noreply.github.com>
2022-07-15 12:48:06 +0530

    Update NiraaliController.java

--- src/main/java/com/example/niraali/NiraaliController.java
+++ src/main/java/com/example/niraali/NiraaliController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class NiraaliController {
 
-	@GetMapping("/checkme")
+	@GetMapping("/getm")
 	public String checkMe() {
 		return "welcome to rest services";
 	}
